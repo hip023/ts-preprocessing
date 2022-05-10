e0ae79c3168247f9b68451c375e2c13e1195599f
hilahersz <hila@trustdome.com>
2022-05-10 21:20:07 +0300

    fix: Plots! FINALLY

--- utilities/plot_ts.py
+++ utilities/plot_ts.py
@@ -5,17 +5,22 @@ import matplotlib.dates as mdates
 
 def plot_series(time,
                 series,
-                value="Value",
-                title=""):
-    plt.grid(True)
+                value=None,
+                title=None):
 
+    if value is None:
+        value = series.name.title()
+    if title is None:
+        title = f"{time.name.title()} VS. {value}: Tiktok Data"
     fig, ax = plt.subplots(figsize=(10, 6))
     ax.plot(time, series,color="purple", alpha=0.5)
-    fig.autofmt_xdate()
 
     ax.set_title(title)
     ax.set_ylabel(value)
     ax.xaxis.set_major_locator(mdates.WeekdayLocator(interval=2))
     ax.xaxis.set_major_formatter(DateFormatter("%m-%d"))
+
+    plt.grid(True)
+    plt.show()
 
     plt.show()
