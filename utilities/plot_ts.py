73776f931562d423f0dc9dd724940d1e562e3882
hilahersz <hila@trustdome.com>
2022-05-10 19:59:03 +0300

    fix plots

--- utilities/plot_ts.py
+++ utilities/plot_ts.py
@@ -1,13 +1,21 @@
 from matplotlib import pyplot as plt
+from matplotlib.dates import DateFormatter
+import matplotlib.dates as mdates
 
 
 def plot_series(time,
                 series,
                 value="Value",
                 title=""):
-    plt.figure(figsize=(10, 6))
-    plt.plot(time, series,color="purple", alpha=0.5)
-    plt.title(title)
-    plt.ylabel(value)
     plt.grid(True)
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(time, series,color="purple", alpha=0.5)
+    fig.autofmt_xdate()
+
+    ax.set_title(title)
+    ax.set_ylabel(value)
+    ax.xaxis.set_major_locator(mdates.WeekdayLocator(interval=2))
+    ax.xaxis.set_major_formatter(DateFormatter("%m-%d"))
+
     plt.show()
