2de9eac388943542fb3dedfc032d9f71bd8abbb1
hilahersz <hila@trustdome.com>
2022-05-14 23:17:45 +0300

    support multiple plots

--- utilities/plot_ts.py
+++ utilities/plot_ts.py
@@ -1,26 +1,39 @@
+import matplotlib.dates as mdates
 from matplotlib import pyplot as plt
 from matplotlib.dates import DateFormatter
-import matplotlib.dates as mdates
+
+COLORS = ["purple", "red", "blue", "green"]
 
 
 def plot_series(time,
-                series,
+                *args,
                 value=None,
-                title=None):
-
-    if value is None:
-        value = series.name.title()
-    if title is None:
-        title = f"{time.name.title()} VS. {value}: Tiktok Data"
+                title=None,
+                labels=None):
     fig, ax = plt.subplots(figsize=(10, 6))
-    ax.plot(time, series,color="purple", alpha=0.5)
+
+    if labels is None:
+        labels = range(len(args))
+
+    for i, series in enumerate(args):
+        ax.plot(time, series, color=COLORS[i], alpha=0.5, label=labels[i])
+        fig.autofmt_xdate()
+
+        ax.xaxis.set_major_locator(mdates.WeekdayLocator(interval=2))
+        ax.xaxis.set_major_formatter(DateFormatter("%m-%d"))
+
+    if value is None and args[0].name:
+        value = args[0].name.title()
+    if title is None and args[0].name and time.name:
+        title = f"{time.name.title()} VS. {value}: Tiktok Data"
 
     ax.set_title(title)
     ax.set_ylabel(value)
-    ax.xaxis.set_major_locator(mdates.WeekdayLocator(interval=2))
-    ax.xaxis.set_major_formatter(DateFormatter("%m-%d"))
 
     plt.grid(True)
+
+    if len(args) > 1:
+        plt.legend()
     plt.show()
 
     plt.show()
